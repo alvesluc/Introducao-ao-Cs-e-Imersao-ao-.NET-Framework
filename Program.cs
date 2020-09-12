8c091208a0ad4d88ca39c5d42190f4273510edde
Lucas <l.alves001@gmail.com>
2020-09-11 23:51:02 -0300

    Sintaxe Básica

--- Program.cs
+++ Program.cs
@@ -4,9 +4,16 @@ namespace ConsoleApp1
 {
     public class Program
     {
-        public static void main(string[] args)
+        public static void Main(string[] args)
         {
+            string name = "Lucas";
+            int age = 19;
+            bool isLearning = true;
 
+            Console.WriteLine(name + " is " + age + " years old.");
+
+            if (isLearning)
+                Console.WriteLine("Trying my best!");
             
             Console.ReadLine();
         }
