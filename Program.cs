246240883b03bcd983fd880314644c3248363430
Lucas <l.alves001@gmail.com>
2020-09-11 23:31:36 -0300

    Sintaxe Básica

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void main(string[] args)
+        {
+
+            
+            Console.ReadLine();
+        }
+    }
+}
