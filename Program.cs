5360c9f62b439de57ab599af03242e185e7a4703
Lucas <l.alves001@gmail.com>
2020-09-12 22:49:23 -0300

    Loops e Iteração

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp1
             int age = 19;
             bool isLearning = true;
 
+            var numbers = new int[] {1,2,3,4};
+
             Console.WriteLine(name + " is " + age + " years old.");
 
             try
@@ -22,6 +24,26 @@ namespace ConsoleApp1
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("for");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("while");
+            int iterator = 0;
+            while (iterator < numbers.Length)
+            {
+                iterator++;
+                Console.WriteLine(iterator);
+            }
+
+            Console.WriteLine("foreach");
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
             }
 
             Console.ReadLine();
